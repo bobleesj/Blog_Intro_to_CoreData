7ec05bf034072369451ab5be3e367ec324447734
Bob Lee <Bob@Bobs-MacBook-Pro.local>
2016-12-28 21:34:46 +0900

    Bob the Developer

--- ToDO App/ViewController.swift
+++ ToDO App/ViewController.swift
@@ -8,18 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+  @IBOutlet weak var tableView: UITableView!
+  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+  var tasks: [Task] = []
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    getData()
+    tableView.reloadData()
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return tasks.count
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    
+    let task = tasks[indexPath.row]
+    
+    if let myName = task.name {
+      cell.textLabel?.text = myName
+    }
+    
+    return cell
+   }
+
+  func getData() {
+    do {
+      tasks = try context.fetch(Task.fetchRequest())
+    }
+    catch {
+      print("Fetching Failed")
+    }
   }
-
-
+  
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+           if editingStyle == .delete {
+      let task = tasks[indexPath.row]
+      context.delete(task)
+      (UIApplication.shared.delegate as! AppDelegate).saveContext()
+      
+      do {
+        tasks = try context.fetch(Task.fetchRequest())
+      }
+      catch {
+        print("Fetching Failed")
+      }
+    }
+    tableView.reloadData()
+  }
 }
 
